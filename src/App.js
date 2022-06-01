f06ec2b8dcd00b5bac6cbc7bc5ffa5b6342476a4
Vadick <vadick1@gmail.com>
2022-06-01 09:11:35 -0500

    iniciando proyecto y agregando estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import Images from './components/Images';
+
 
 function App() {
+  const apiKey = 'VnXrGiBX74hlQX19xUmi6T3XZuLlnpulBxWuIxWkRbE'
+  const url = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=10`;
+
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setImages(data))
+    }, [])
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+      <Images images={images}/>
+    </>
+   
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <h1>Scroll Infinito</h1>
+            <h2>Pruebalo!!</h2>
+        </div>       
+    )
+}
+
+export default Header
